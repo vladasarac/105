3fb2449ecf88162de589644dcd65250677f84ccd
Vladimir Sarac <vsarac@spel.local>
2018-10-24 10:00:39 +0200

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path = require('path');
+const http = require('http');
 const publicPath = path.join(__dirname, '../public');//ovo je samo da i napravili cistiju i kracu putanju do public foldera(glupost)
-const express = require('express');
-const port = process.env.PORT || 3000;
 // console.log(publicPath);
+const express = require('express');
+const socketIO = require('socket.io');
+const port = process.env.PORT || 3000;
+
 
 var app = express();
-
+var server = http.createServer(app);//pravimo http server i kao argument dajemo app tj express server
+var io = socketIO(server);
 app.use(express.static(publicPath));
 
-app.listen(3000, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+  	console.log('User is disconnected');
+  });	
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);	
 });
 
